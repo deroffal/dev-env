b4f722f4832c140845af2315adaf3e6b7fffc646
deroffal <alexandre.deroff@protonmail.com>
2021-12-23 12:42:10 +0100

    Init aliases documentation

---
+++ powershell-alias/profile.ps1
@@ -0,0 +1,79 @@
+# Location
+function CdWorkspace([string] $directory)
+{
+    Set-Location -Path "$Env:w\$directory"
+}
+
+Set-Alias -Name cdw -Value CdWorkspace
+
+# Java
+function SetJava8
+{
+    $Env:JAVA_HOME = $Env:JAVA8_HOME
+    $Env:PATH = "$Env:JAVA_HOME\bin;$Env:PATH"
+    java -version
+}
+
+function SetJava11
+{
+    $Env:JAVA_HOME = $Env:JAVA11_HOME
+    $Env:PATH = "$Env:JAVA_HOME\bin;$Env:PATH"
+    java -version
+}
+
+function SetJava14
+{
+    $Env:JAVA_HOME = $Env:JAVA14_HOME
+    $Env:PATH = "$Env:JAVA_HOME\bin;$Env:PATH"
+    java -version
+}
+
+function SetJava17
+{
+    $Env:JAVA_HOME = $Env:JAVA17_HOME
+    $Env:PATH = "$Env:JAVA_HOME\bin;$Env:PATH"
+    java -version
+}
+
+Set-Alias -Name java8 -Value SetJava8
+Set-Alias -Name java11 -Value SetJava11
+Set-Alias -Name java14 -Value SetJava14
+Set-Alias -Name java17 -Value SetJava17
+
+# Maven
+function mavenCleanInstall
+{
+    <#
+        .SYNOPSIS
+            Run a mvn clean install.
+
+        .EXAMPLE
+            mci -f parent/pom.xml -st
+    #>
+    Param(
+        #Force the use of an alternate POM file (or directory with pom.xml) (cf -f in mvn)
+        [Parameter(Mandatory = $false)]
+        [string]$f,
+        #If present, skip tests (add -DskipTests to the command)
+        [Parameter(Mandatory = $false)]
+        [switch]$st
+    )
+
+    $command = "mvn clean install"
+
+    if ($f)
+    {
+        $command = "$command -f $f"
+    }
+
+    if ($st)
+    {
+        $command = "$command -DskipTests"
+    }
+
+
+    Write-Output $command
+    Invoke-Expression $command
+}
+
+Set-Alias -Name mci -Value mavenCleanInstall
